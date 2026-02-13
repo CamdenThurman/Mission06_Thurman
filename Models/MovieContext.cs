34a779f82ccc15c414ba0c9a10c1b659616ae2af
CamdenThurman <camden.thurman@gmail.com>
2026-02-13 14:56:19 -0700

    Add project files.

---
+++ Models/MovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Mission06_YourLastName.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
